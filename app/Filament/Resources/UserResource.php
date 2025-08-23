0d6ef5dab17eebc23798a89804fa26546a028c78
muslichwr <muslichwahyuromadhon@gmail.com>
2025-08-23 20:10:48 +0700

    modify UserResource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,8 +18,15 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
+    protected static ?int $navigationSort = 1;
+
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Manajemen Pengguna';
+    }
+    
     public static function form(Form $form): Form
     {
         return $form
